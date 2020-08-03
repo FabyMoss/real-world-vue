7302c910aacac967997b65c117d7c164d9c6e9a6
Mos Fabian <fabyfabi25@gmail.com>
2020-08-03 19:29:34 +0300

    Lectia 6,7 si 8 inclusiv(Icon, slots, EventCard)

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link>|
+    <EventCard />
+    <BaseIcon />
   </div>
 </template>
+
+<script>
+import EventCard from 'C:/Users/user/real-world-vue/src/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
